1c35735368baacb49c0582d15b751daf65eaa5c8
Nels885 <voirin.lionel@gmail.com>
2019-01-24 15:42:03 +0100

    fix STATIC_ROOT

--- disquaire_project/settings/__init__.py
+++ disquaire_project/settings/__init__.py
@@ -136,4 +136,4 @@ STATIC_URL = '/static/'
 # Django debug toolbar
 INTERNAL_IPS = ['127.0.0.1']
 
-STATIC_ROOT = os.path.join(BASE_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
